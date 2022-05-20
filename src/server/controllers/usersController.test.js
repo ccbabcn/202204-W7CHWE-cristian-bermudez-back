aae882c3ead9cc4aa31acd12185d9270c6286a32
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-20 23:31:09 +0200

    implemented corner cases for loadUser controller test

--- src/server/controllers/usersController.test.js
+++ src/server/controllers/usersController.test.js
@@ -7,6 +7,7 @@ describe("given loadUsers function", () => {
     status: jest.fn().mockReturnThis(),
     json: jest.fn(),
   };
+
   describe("When it's invoked with a response", () => {
     test("Then it should call the response status method with 200 and return the user", async () => {
       User.find = jest.fn().mockResolvedValue(mockUsers);
@@ -38,6 +39,32 @@ describe("given loadUsers function", () => {
 
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
       expect(res.json).toHaveBeenCalledWith(expectedUsers);
+    });
+  });
+
+  describe("When it's invoke with a response but cant find any user", () => {
+    const newError = new Error();
+    newError.code = 400;
+    newError.errorMessage = "Bad request";
+
+    test("Then it should create a new Error and call next with the new error", async () => {
+      User.find = jest.fn().mockResolvedValue(undefined);
+      const next = jest.fn().mockReturnThis();
+
+      await loadUsers(null, res, next);
+
+      expect(next).toHaveBeenCalledWith(newError);
+    });
+  });
+
+  describe("When it's invoked with a response but an error occurs", () => {
+    test("Then it should call next", async () => {
+      User.find = jest.fn().mockRejectedValue();
+      const next = jest.fn().mockReturnThis();
+
+      await loadUsers(null, res, next);
+
+      expect(next).toHaveBeenCalled();
     });
   });
 });
