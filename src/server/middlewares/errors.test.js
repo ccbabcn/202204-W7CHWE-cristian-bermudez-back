51ba8a218fec384003c89aab8e9b556be639cd8d
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-21 01:41:35 +0200

    implemented generalError custom error test

--- src/server/middlewares/errors.test.js
+++ src/server/middlewares/errors.test.js
@@ -1,5 +1,4 @@
-const { json } = require("express/lib/response");
-const { notFoundError } = require("./errors");
+const { notFoundError, generalError } = require("./errors");
 
 describe("Given a notFounError function", () => {
   describe("When it's invoke with a response", () => {
@@ -18,6 +17,29 @@ describe("Given a notFounError function", () => {
 
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
       expect(res.json).toHaveBeenCalledWith(expectedMessage);
+    });
+  });
+});
+
+describe("Given a generalError function", () => {
+  describe("When it's invoke with a response and a error with errorCode 405 and a errorMessage 'Custom Error Mesage'", () => {
+    test("Then it should call the response status methos with 405 and the json method with the passes message", () => {
+      const receivedError = new Error();
+      receivedError.code = 405;
+      receivedError.message = "Custom Error Mesage";
+
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      const expectedStatus = 405;
+      const expectedMessage = { message: "Custom Error Mesage" };
+
+      generalError(receivedError, null, res);
+
+      expect(res.status).toBeCalledWith(expectedStatus);
+      expect(res.json).toBeCalledWith(expectedMessage);
     });
   });
 });
